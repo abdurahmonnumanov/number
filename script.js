6e621cb40b849b4b767f6c1e5fc3d83883a9e6eb
abdurahmon <13@.ru>
2022-05-09 11:06:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var a = prompt('Введите ваше имя')
+var b = prompt('Введите ваш возрост')
+console.log(a);
+console.log(b);
+var myNumber = +prompt('10+3')
+console.log('10+3 =' + (10+3) + ' Ваш ответ ' + myNumber);
+var myNumbe = +prompt('10-3')
+console.log('10-3 =' + (10-3) + ' Ваш ответ ' + myNumbe);
+var myNumb = +prompt('10*3')
+console.log('10*3 =' + (10*3) +  ' Ваш ответ ' + myNumb);
+var myNum = +prompt('10%3')
+console.log('10%3 =' + (10%3) + ' Ваш ответ ' + myNum);
+alert('Посмотрите в консоль')
